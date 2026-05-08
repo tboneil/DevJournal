9dfcbe6202dc0a2c5f6eb5677e0b8fc1e87d9b3c
Tyler O'Neil <oneil_tyler@icloud.com>
2026-05-07 21:24:58 -0500

    Created a cardbuttonview to have selectable buttons for a main page to do quick operations.

--- DevJournal/Views/MainView.swift
+++ DevJournal/Views/MainView.swift
@@ -9,8 +9,44 @@ import SwiftUI
 
 struct MainView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        // TODO: Add in NavigationSplitMenu with links for some other abilities
+        
+        Grid(horizontalSpacing: 20, verticalSpacing: 20) {
+            GridRow {
+                CardButtonView(title: "Create New Entry", icon: "pencil", action: {})
+                    .padding()
+                CardButtonView(title: "Export", icon: "square.and.arrow.up", action: {})
+                    .padding()
+            }
+            GridRow {
+                // Place cards here
+                CardButtonView(title: "Quick Entry", icon: "pencil", action: {})
+                    .padding()
+                CardButtonView(title: "List Entries", icon: "list.dash", action: {})
+                    .padding()
+            }
+        }
+        
     }
+}
+
+// TODO: Define functions that call views based on card selection
+
+private func callEntryEditorView() {
+    
+}
+
+private func callQuickEntryView() {
+    
+}
+
+private func callListView() {
+    
+}
+
+private func callExportView() {
+    
 }
 
 #Preview {
