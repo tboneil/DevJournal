ee50ed99f5ff8bd4fad887dc2e71009354e0961e
Tyler O'Neil <oneil_tyler@icloud.com>
2026-05-11 09:26:33 -0500

    Scratching UI in dev to rebuild and redesign in a cleaner way after not liking the views I defined before. Goal is try and refine the options, limit the amount view files required and utilize menus and buttons more.

--- DevJournal/Views/MainView.swift
+++ DevJournal/Views/MainView.swift
@@ -7,54 +7,12 @@
 
 import SwiftUI
 
-enum NavigationDestination: Hashable {
-    case entryEditor
-    case listEntries
-    case exportView
-}
-
 struct MainView: View {
     @State private var navigationPath = NavigationPath()
     var body: some View {
-        
-        NavigationStack(path: $navigationPath) {
-            Grid(horizontalSpacing: 20, verticalSpacing: 20) {
-                GridRow {
-                    CardButtonView(title: "Create New Entry", icon: "pencil") {
-                        navigate(to: .entryEditor)
-                    }
-                        .padding()
-                    CardButtonView(title: "Export", icon: "square.and.arrow.up") {
-                        navigate(to: .exportView)
-                    }
-                        .padding()
-                }
-                GridRow {
-                    CardButtonView(title: "List Entries", icon: "list.dash") {
-                        navigate(to: .listEntries)
-                    }
-                        .padding()
-                }
-            }
-            .navigationDestination(for: NavigationDestination.self) { destination in
-                switch destination {
-                case .entryEditor:
-                    EntryEditorView()
-                case .listEntries:
-                        ListEntryView()
-                case .exportView:
-                    ExportEntryToFileView(entriesToExport: nil)
-                        
-                }
-            }
-        }
-        
+        Text("Hello World")
     }
-    
-    // TODO: Define functions that call views based on card selection
-    private func navigate(to destination: NavigationDestination) {
-        navigationPath.append(destination)
-    }
+        
 }
 
 
